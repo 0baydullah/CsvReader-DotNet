6ba3bff6c391acd9cf63795c6fb46d86a546b691
Obaydullah <obaydullahkhaan@gmail.com>
2025-01-10 15:31:04 +0600

    initial commit

---
+++ CsvReader/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CsvReader.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
